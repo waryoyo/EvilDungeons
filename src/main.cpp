90e9e007b9e5d20ce8f2eddf1e6a032a6dfa3292
waryoyo <youssef_hus@hotmail.com>
2025-04-17 19:05:19 +0200

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+﻿// EvilDungeons.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
