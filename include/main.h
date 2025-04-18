b8e9ae75ef985b874d252f55c3a1c5d4519c2da4
waryoyo <youssef_hus@hotmail.com>
2025-04-19 01:10:37 +0200

    added mingw64 preset

--- include/main.h
+++ include/main.h
@@ -1,7 +1,4 @@
-﻿// EvilDungeons.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
 #include <glm/common.hpp>
@@ -9,4 +6,3 @@
 #include <imgui.h>
 #include <GLFW/glfw3.h>
 
-// TODO: Reference additional headers your program requires here.
